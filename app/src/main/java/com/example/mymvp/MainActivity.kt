b9b4b37a7ad8b955be8cfdbe6e9bdd2b9df36661
docAlexxx <docalex@mail.ru>
2022-08-08 22:03:24 +0400

    Lesson1 finish

--- app/src/main/java/com/example/mymvp/MainActivity.kt
+++ app/src/main/java/com/example/mymvp/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.mymvp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mymvp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), MainView {
